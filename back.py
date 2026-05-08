f35c8ed3de1ed48ca2ba9e118e7110d3bf183c18
JohnnyWorku <Johnnythegold41@gmail.com>
2026-05-08 15:08:06 +0300

    the backend updated to handle pydantic

--- back.py
+++ back.py
@@ -3,12 +3,16 @@ from dotenv import load_dotenv
 # from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 from langchain_neo4j import Neo4jGraph
+from langchain_core.output_parsers import PydanticOutputParser
+
+
+from query_validator import CypherQuery
+
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Intialize the Google Generative AI model
-# llm = ChatGoogleGenerativeAI(model=os.getenv("GROQ_MODEL"), google_api_key=os.getenv("GROQ_API_KEY"))
+
 llm = ChatGroq(
     model=os.getenv("GROQ_MODEL", "llama-3.1-8b-instant"),
     api_key=os.getenv("GROQ_API_KEY")
@@ -21,11 +25,10 @@ graph = Neo4jGraph(
     username = os.getenv("LOCAL_NEO4J_USERNAME"),
     password= os.getenv("LOCAL_NEO4J_PASSWORD"),
     database=os.getenv("LOCAL_NEO4J_DB")
-    
-    # url = os.getenv("REMOTE_NEO4J_URI"),
-    # username = os.getenv("REMOTE_NEO4J_USERNAME"),
-    # password= os.getenv("REMOTE_NEO4J_PASSWORD"),
-    # database=os.getenv("REMOTE_NEO4J_DB")
+)
+
+parser = PydanticOutputParser(
+    pydantic_object=CypherQuery
 )
 
 
@@ -40,29 +43,35 @@ def generate_cypher(question, schema):
     shcema:
     {schema}
     
-    Convert the question into Cypher query.
-    Only return the query.
-    No markdown(```).
-    No any other elaboration just the cypher only.
+    Convert the user question into a valid Cypher query.
     
-    Question:
+    Rules:
+    - Use ONLY schema elements provided
+    - Return ONLY JSON
+    - No markdown
+    - No explanation
+    - No dangerous operations
+    
+    {parser.get_format_instructions()}
+    
+    
+    User Question:
     {question}
     """
     
     response = llm.invoke(prompt)
-    content = response.content.strip()
+    parsed_content = parser.parse(response.content)
         
-    return content
+    return parsed_content.cypher
 
 
-def validate_cypher(cypher: str):
-    forbidden = ["DELETE", "CREATE", "MERGE", "DROP"]
-    
-    for word in forbidden:
-        if word in cypher.upper():
-            raise ValueError("Dangerous query detected.")
+# To validate its syntax correctness
+def validate_with_neo4j(cypher: str):
+    try:
+        graph.query(f"EXPLAIN {cypher}")
         
-    return cypher
+    except Exception as e:
+        raise ValueError(f"Neo4j syntax validation failed: {str(e)}")
 
 
 def run_query(cypher):
@@ -76,14 +85,14 @@ def format_results(results):
     formatted = []
 
     for row in results:
-        formatted.append(", ".join(str(value) for value in row.values()))
+        formatted.append(", ".join(f"{key}: {value}" for key, value in row.items()))
         
     return "\n".join(formatted)
 
 
 def generate_humanized_answer(question, context):
     prompt = f"""
-    Answer the question using the data below.
+    Answer the user's question using ONLY the provided data.
     
     Question:
     {question}
@@ -95,7 +104,7 @@ def generate_humanized_answer(question, context):
     """
     
     response = llm.invoke(prompt)
-    return response.content
+    return response.content.strip()
     
     
 def ask_the_graph(question):
@@ -103,14 +112,17 @@ def ask_the_graph(question):
         schema = get_schema()
         
         cypher = generate_cypher(question, schema)
+        
         print("\nGenerated cypher:\n", cypher)
         
-        cypher = validate_cypher(cypher)
+        validate_with_neo4j(cypher)
         
         results = run_query(cypher)
+        
         print("\nRaw results:\n", results)
         
         context = format_results(results)
+        
         print("\nContext:\n", context)
         
         final_answer = generate_humanized_answer(question, context)
@@ -119,33 +131,9 @@ def ask_the_graph(question):
     
     except Exception as e:
         return f"""
-            Error: The statement you entered is either not changeable to cypher or it has some flaw. Check it.
-            If you want see the original error: {str(e)}
+            Error: The generated Cypher query was invalid or unsafe.
+
+            Details:
+                {str(e)}
         """
-    
 
-# chain = GraphCypherQAChain.from_llm(
-#     llm=llm,
-#     graph=graph,
-#     verbose=True,
-#     allow_dangerous_requests=True
-# )
-
-
-# def main_agent():
-#     try:
-#         print("The agent is running ...: Type 'exit/quit' to stop the agent.")
-#         while True:
-#             user_input = input("You: ")
-#             if user_input.lower() in ["exit", "quit"]:
-#                 print("Exting the agent. Goodbye!")
-#                 break
-            
-#             response = chain.invoke({"query": user_input})
-#             print(f"AI agent: {response}")
-            
-#     except Exception as e:
-#         print(f"An error occured: {e}")
-    
-# if __name__ == "__main__":
-#     main_agent()
